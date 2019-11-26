96ea169f7f0cdb8d59cc9524a209208898a860ba
福岡優貴 <fukuokayuuki@fukuokayuukinoMacBook-Pro.local>
2019-11-26 17:05:12 +0900

    View fix2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-Category.find_or_create_by(name:"life")
-Category.find_or_create_by(name:"hobby")
-Category.find_or_create_by(name:"other")
-Category.find_or_create_by(name:"work")
+Category.find_or_create_by(name:"生活")
+Category.find_or_create_by(name:"趣味")
+Category.find_or_create_by(name:"仕事")
+Category.find_or_create_by(name:"その他")
