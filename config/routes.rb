8fcfc399cd2048ba6b9d4420a73a47fbc02491ce
福岡優貴 <fukuokayuuki@fukuokayuukinoMacBook-Pro.local>
2019-12-11 00:57:42 +0900

    Add category layout

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   delete "/memos/:id", to:"memos#destroy"
   get"/memos/:id/edit",to:"memos#edit"
   patch "/memos/:id",to:"memos#update"
-  get"/categories/:id".to:"categories#show"
+  get"/categories/:id",to:"categories#show"
 
 end
